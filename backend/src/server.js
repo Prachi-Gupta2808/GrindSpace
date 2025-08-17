b3cf34b7ddf16f8f205893e79eee5b177122d7c0
Prachi-Gupta2808 <prachig2808@gmail.com>
2025-08-17 17:45:33 +0530

    backend stuff

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["grind-space.vercel.app"],
     credentials: true //alow frontend to send cookies
 }))
 app.use(express.json());
